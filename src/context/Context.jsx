f27f7ef16cb02e7f5a06c5b4234894f1d4339b5a
Justus78 <stars787878@aol.com>
2024-11-11 22:22:22 -0500

    added project to github

---
+++ src/context/Context.jsx
@@ -0,0 +1,89 @@
+import { createContext, useEffect, useState } from "react";
+import runChat from "../config/gemini";
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResults] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const delayPara = (index, nextWord) => {
+    setTimeout(function () {
+      setResultData((prev) => prev + nextWord);
+    }, 75 * index);
+  };
+
+  const newChat = () => {
+    setLoading(false);
+    setShowResults(false);
+  };
+
+  // Function to handle sending the prompt to the chat
+  const onSent = async (prompt) => {
+    //console.log(input);
+
+    setResultData(""); // reset the resultData variable
+    setLoading(true); // start loading animation
+    setShowResults(true); // show results
+    let response;
+    if (prompt !== undefined) {
+      response = await runChat(prompt);
+      setRecentPrompt(prompt);
+    } else {
+      setPrevPrompts((prev) => [...prev, input]);
+      setRecentPrompt(input);
+      response = await runChat(input);
+    }
+
+    //setRecentPrompt(input); // sets the current input state to the recent prompt state
+    //setPrevPrompts((prev) => [...prev, input]);
+
+    //const response = await runChat(input); // call the runChat function and set the response to a variable
+
+    let responseArray = response.split("**");
+    let newResponse = "";
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+
+    let newResponse2 = newResponse.split("*").join("<br/>");
+
+    let newResponseArray = newResponse2.split(" ");
+
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextWord = newResponseArray[i];
+      delayPara(i, nextWord + " ");
+    }
+    setLoading(false); // end loading animation
+    setInput(""); // reset input box
+  };
+
+  //onSent("What is Google Gemini?"); // works and gets a response
+
+  const contextValue = {
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+    newChat,
+  };
+
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
